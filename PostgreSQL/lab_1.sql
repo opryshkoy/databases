9ac9c95e8e7931b3d352995445de76f652baac80
opryshkoy <Oo.juliya@gmail.com>
2019-01-30 07:59:14 -0800

    добавила DROP TABLE IF EXISTS, заполнила таблицы данными

--- PostgreSQL/lab_1.sql
+++ PostgreSQL/lab_1.sql
@@ -1,6 +1,8 @@
+DROP TABLE IF EXISTS films, persons, persons2content;
+
 CREATE TABLE films (
-	title VARCHAR (128) NOT NULL,
 	id SERIAL PRIMARY KEY,
+	title VARCHAR (128) NOT NULL,
 	country VARCHAR (128) NOT NULL,
 	box_office MONEY DEFAULT 0,
 	release_year TIMESTAMP
@@ -20,4 +22,28 @@ CREATE TABLE persons2content (
 );
 
 
+INSERT INTO films (title, country, box_office, release_year) VALUES 
+('Зеленая миля', 'США', 286801374, '1999-12-06'), 
+('Побег из Шоушенка', 'США', 59841469, '1994-09-10'),
+('Форрест Гамп', 'США', 677386686, '1994-06-23'),
+('Авиатор', 'Германия,США', 213741459, '2004-12-14'),
+('Остров проклятых', 'США', 294804195, '2010-02-13');
 
+INSERT INTO persons (first_name, second_name) VALUES 
+('Том', 'Хэнкс'), 
+('Тим', 'Роббинс'),
+('Том', 'Хэнкс'),
+('Леонардо', 'ДиКаприо'),
+('Леонардо', 'ДиКаприо');
+
+INSERT INTO persons2content (person_id, film_id, person_type) VALUES 
+(1, 1, 'Актер'), 
+(2, 2, 'Актер'), 
+(3, 3, 'Актер'), 
+(4, 4, 'Актер'), 
+(5, 5, 'Актер');
+
+
+SELECT * FROM films;
+SELECT * FROM persons;
+SELECT * FROM persons2content;
